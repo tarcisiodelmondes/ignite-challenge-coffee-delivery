b9e1016819a3fb38c95a2508e55a9a3d40051de5
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-07-29 19:31:44 -0300

    feat: adding DefaultLayout component

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const LayoutContainer = styled.div`
+  max-width: 74rem;
+  margin: 0 auto;
+  padding: 2rem;
+
+  display: flex;
+  flex-direction: column;
+`;
