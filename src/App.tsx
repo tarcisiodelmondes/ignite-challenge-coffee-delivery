a7eba6907f2bef6a3d84deafa2e8fdda10c8261e
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-07-29 19:32:32 -0300

    feat: setting react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import { Router } from "./Router";
 import { GlobalStyles } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>Hello</div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyles />
     </ThemeProvider>
