b3fd12db1cf14d96afe733cc0a286c1d61a7da63
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-08-04 15:46:37 -0300

    adjust: Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,13 @@
 import { MapPin, ShoppingCart } from "phosphor-react";
 import LogoCoffeeDelivery from "../../assets/coffee-delivery-logo.svg";
+import { useCart } from "../../hooks/cart";
 import { CartContainer, HeaderContainer, LocationContainer } from "./styles";
 
 export function Header() {
+  const { cart } = useCart();
+
+  const totalProductInCart = cart.length;
+
   return (
     <HeaderContainer>
       <img src={LogoCoffeeDelivery} alt="" />
@@ -14,8 +19,10 @@ export function Header() {
           <span>Porto Alegre, RS</span>
         </LocationContainer>
 
-        <CartContainer>
+        <CartContainer to="/checkout">
           <ShoppingCart size={20} weight="fill" />
+
+          {totalProductInCart > 0 ? <span>{totalProductInCart}</span> : null}
         </CartContainer>
       </div>
     </HeaderContainer>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
+  max-width: 74rem;
+  width: 100%;
+  margin: 0 auto;
+  padding: 2rem;
+
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -30,7 +36,7 @@ export const LocationContainer = styled.div`
   justify-content: center;
 `;
 
-export const CartContainer = styled.div`
+export const CartContainer = styled(Link)`
   border-radius: 6px;
   max-height: 2.375rem;
   padding: 0.5rem;
@@ -40,4 +46,33 @@ export const CartContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
+
+  transition: filter 300ms;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(0.9);
+  }
+
+  & > span {
+    position: absolute;
+    top: -0.5rem;
+    right: -0.5rem;
+    background: ${({ theme }) => theme["yellow-700"]};
+    border-radius: 50%;
+    color: ${({ theme }) => theme["white"]};
+
+    height: 1.25rem;
+    width: 1.25rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: 0.75rem;
+    font-weight: 700;
+
+    letter-spacing: -0.06em;
+  }
 `;
