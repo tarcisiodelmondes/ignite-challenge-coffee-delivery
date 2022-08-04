51dfddae30178cecd2c7bc5f1bedd04c40643e3f
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-08-04 15:47:17 -0300

    adjust: DefaultLayout component

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
-  max-width: 74rem;
   margin: 0 auto;
-  padding: 2rem;
 
   display: flex;
   flex-direction: column;
